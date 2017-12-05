b8ec09ae54b0bdbf969f18a2c42c32a629a7285d
郑宇鹏 <1121920909@qq.com>
2017-12-05 16:26:30 +0800

    add CloudSimExample1

--- cloudSimGUI/src/main/java/GUI/MainForm.java
+++ cloudSimGUI/src/main/java/GUI/MainForm.java
@@ -1,17 +1,28 @@
 package GUI;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainForm {
     private JPanel title;
     private JPanel centerPanel;
     private JPanel labelPanel;
     private JPanel buttonPanel;
-    private JButton ENTERButton;
-    private JButton ENTERButton1;
+    private JButton exampleButton;
+    private JButton simulationButton;
     private JLabel exampleLabel;
     private JLabel simulationLabel;
     private JPanel mainPanel;
+
+    public MainForm() {
+        exampleButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
     public  void test() {
         JFrame frame = new JFrame("MainForm");
         frame.setContentPane(new MainForm().mainPanel);
